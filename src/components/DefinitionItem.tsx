43ba1d7b41c92fb68cd6608c7b254f4bab976015
Mykyta Palamarchuk <mpalamarchuk2019@fau.edu>
2025-08-17 20:00:21 -0400

    Added Margins to Attributes

--- src/components/DefinitionItem.tsx
+++ src/components/DefinitionItem.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const DefinitionItem = ({ term, children }: Props) => {
   return (
-    <Box>
+    <Box marginY={5}>
       <Heading as="dt" fontSize="md" color="gray.600">
         {term}
       </Heading>
