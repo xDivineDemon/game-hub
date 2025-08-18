ad6203d69ac9b4ee4371505993f6f463adc4446e
Mykyta Palamarchuk <mpalamarchuk2019@fau.edu>
2025-08-17 20:23:16 -0400

    Build Game Screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,13 @@
+import APIClient from "../services/api-client";
+import { Screenshot } from "../entities/Screenshot";
+import { useQuery } from "@tanstack/react-query";
+
+const useScreenshots = (gameId: number) => {
+  const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiClient.getAll,
+  });
+};
+
+export default useScreenshots;
